8d439541ec565b8b0d056d04c5b69cf74f127c9a
Alexi <108108603+itsalexi@users.noreply.github.com>
2023-02-18 15:18:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -9,101 +9,108 @@ import Snowfall from 'react-snowfall';
 
 function App() {
     return (
-        <div className="container">
-            <Snowfall color="lightpink" />
-            <div className="app">
-                <div className="nav">
-                    <h3>HOME</h3>
-                    <div className="title-container">
-                        <h1 className="title">Kanade's</h1>
+        <>
+            <Snowfall
+                style={{
+                    height: '200vh',
+                }}
+                color="cyan"
+            />
+            <div className="container">
+                <div className="app">
+                    <div className="nav">
+                        <h3>HOME</h3>
+                        <div className="title-container">
+                            <h1 className="title">Kanade's</h1>
+                        </div>
+                        <h3>CART</h3>
                     </div>
-                    <h3>CART</h3>
+                    <div className="main-content">
+                        <div className="collection unique">
+                            <div className="collection-information">
+                                <div className="collection-text">
+                                    <h2 className="collection-title unique">
+                                        Unique
+                                    </h2>
+                                    <h1 className="collection-subtitle unique">
+                                        Collection
+                                    </h1>
+                                </div>
+                                <button className="collection-shop-button">
+                                    SHOP NOW
+                                </button>
+                            </div>
+                            <img
+                                src={model1}
+                                className="collection-model"
+                                alt="model1"
+                            />
+                        </div>
+                        <div className="collection seasonal">
+                            <img
+                                src={model2}
+                                className="collection-model"
+                                alt="model2"
+                            />
+                            <div className="collection-information">
+                                <div className="collection-text">
+                                    <h2 className="collection-title seasonal">
+                                        Seasonal
+                                    </h2>
+                                    <h1 className="collection-subtitle seasonal">
+                                        Collection
+                                    </h1>
+                                </div>
+                                <button className="collection-shop-button">
+                                    SHOP NOW
+                                </button>
+                            </div>
+                        </div>
+                        <div className="collection casual">
+                            <div className="collection-information">
+                                <div className="collection-text">
+                                    <h2 className="collection-title casual">
+                                        Casual
+                                    </h2>
+                                    <h1 className="collection-subtitle">
+                                        Collection
+                                    </h1>
+                                </div>
+                                <button className="collection-shop-button">
+                                    SHOP NOW
+                                </button>
+                            </div>
+                            <img
+                                src={model3}
+                                className="collection-model"
+                                alt="model1"
+                            />
+                        </div>
+                        <div className="collection winter">
+                            <img
+                                src={model4}
+                                className="collection-model"
+                                alt="model1"
+                            />
+                            <div className="collection-information">
+                                <div className="collection-text">
+                                    <h2 className="collection-title winter">
+                                        Winter
+                                    </h2>
+                                    <h1 className="collection-subtitle">
+                                        Collection
+                                    </h1>
+                                </div>
+                                <button className="collection-shop-button">
+                                    SHOP NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="main-content">
-                    <div className="collection unique">
-                        <div className="collection-information">
-                            <div className="collection-text">
-                                <h2 className="collection-title unique">
-                                    Unique
-                                </h2>
-                                <h1 className="collection-subtitle unique">
-                                    Collection
-                                </h1>
-                            </div>
-                            <button className="collection-shop-button">
-                                SHOP NOW
-                            </button>
-                        </div>
-                        <img
-                            src={model1}
-                            className="collection-model"
-                            alt="model1"
-                        />
-                    </div>
-                    <div className="collection seasonal">
-                        <img
-                            src={model2}
-                            className="collection-model"
-                            alt="model2"
-                        />
-                        <div className="collection-information">
-                            <div className="collection-text">
-                                <h2 className="collection-title seasonal">
-                                    Seasonal
-                                </h2>
-                                <h1 className="collection-subtitle seasonal">
-                                    Collection
-                                </h1>
-                            </div>
-                            <button className="collection-shop-button">
-                                SHOP NOW
-                            </button>
-                        </div>
-                    </div>
-                    <div className="collection casual">
-                        <div className="collection-information">
-                            <div className="collection-text">
-                                <h2 className="collection-title casual">
-                                    Casual
-                                </h2>
-                                <h1 className="collection-subtitle">
-                                    Collection
-                                </h1>
-                            </div>
-                            <button className="collection-shop-button">
-                                SHOP NOW
-                            </button>
-                        </div>
-                        <img
-                            src={model3}
-                            className="collection-model"
-                            alt="model1"
-                        />
-                    </div>
-                    <div className="collection winter">
-                        <img
-                            src={model4}
-                            className="collection-model"
-                            alt="model1"
-                        />
-                        <div className="collection-information">
-                            <div className="collection-text">
-                                <h2 className="collection-title winter">
-                                    Winter
-                                </h2>
-                                <h1 className="collection-subtitle">
-                                    Collection
-                                </h1>
-                            </div>
-                            <button className="collection-shop-button">
-                                SHOP NOW
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                <div className="footer">Made by Alexi</div>
             </div>
-            <div className="footer">Made by Alexi</div>
-        </div>
+        </>
     );
 }
 
